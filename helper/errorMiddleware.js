8135006ea0c59b6a972abca3c62693c92ec22390
AJAS M A <123362772+ajasma6570@users.noreply.github.com>
2023-11-26 01:16:50 +0530

    Update errorMiddleware.js

--- helper/errorMiddleware.js
+++ helper/errorMiddleware.js
@@ -1,45 +1,25 @@
 // helper/errorMiddleware.js
-// const express=require('express')
+const express=require('express')
 
-// const app=express()
+const app=express()
 // Set the view engine to use EJS
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // Set the 'views' directory to the root 'views' folder
-// app.set('views', './views/userViews');
+app.set('views', './views/userViews');
 
 // Middleware to handle unhandled routes
-//*
-//*
-//*
-// const notFoundHandler = (req, res, next) => {
-//   const error = new Error('Page not found');
-//   error.status = 404;
-//   next(error);
-// };
-
-// // Error handling middleware to display the error message
-// const errorHandler = (error, req, res, next) => {
-//   const status = error.status || 500;
-//   const message = error.message || 'Something went wrong';
-  
-//   // Log the error stack trace
-//   console.error(error.stack);
-//   res.status(status).render('error', { error: message }); // Render 'error.ejs' template with the error message
-// };
-//*
-//*
-//*
 const notFoundHandler = (req, res, next) => {
   const error = new Error('Page not found');
   error.status = 404;
   next(error);
 };
 
+// Error handling middleware to display the error message
 const errorHandler = (error, req, res, next) => {
   const status = error.status || 500;
   const message = error.message || 'Something went wrong';
-
+  
   // Log the error stack trace
   console.error(error.stack);
   res.status(status).render('error', { error: message }); // Render 'error.ejs' template with the error message
